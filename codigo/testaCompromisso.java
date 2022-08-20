f60cfd28c0d30c4f656b1ff0d43702bb18064d10
Lucas Picinin <lucaspicinin@hotmail.com>
2022-08-20 19:07:00 -0300

    fazendo testes de compromisso

---
+++ codigo/testaCompromisso.java
@@ -0,0 +1,23 @@
+import Compromisso.java;
+
+
+class testaCompromisso {
+
+    
+    @Test 
+    public void testConstrutor() {
+        
+    }
+
+    @Test 
+    public void testConstrutor2() {
+        
+    }
+
+    @Test 
+    public void testConstrutor3() {
+        
+    }
+
+    
+}
