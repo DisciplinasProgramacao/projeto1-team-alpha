e6984706b186abf94a7eb45a7a730a519711b1c7
Nikolas Louret <navlouret@sga.pucminas.br>
2022-08-22 09:02:58 -0300

    [Update] Adição dos imports de teste e criação da pasta de testes #9

--- codigo/test/testaCompromisso.java
+++ codigo/test/testaCompromisso.java
@@ -1,9 +1,14 @@
+package test;
+
 import Compromisso.java;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Teste da classe Compromisso
+ */
+class testCompromisso {
 
-class testaCompromisso {
-
-    
     @Test 
     public void testConstrutor() {
         
