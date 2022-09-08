224b0a5da5d31e14cb132db20e4d541be4bd357c
Gabriel Lima de Souza <90854484+GabrielLimaDeSouza@users.noreply.github.com>
2022-09-08 20:28:59 -0300

    Update Data.java

--- codigo/Data.java
+++ codigo/Data.java
@@ -1,17 +1,16 @@
 import java.time.LocalDateTime;
 
 /** Classe data simples para fundamentos de POO */
-public class Data {
+public class Data{
     //#region atributos
+    //constante: dias de cada mês
+    private static final int[] DIASDOMES = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    private static final String[] DIASDASEMANA = {"Sábado", "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"}; //2022 começa em um sábado
 
-    // constante: dias de cada mês
-    private static final int[] DIASDOMES = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    private static final String[] DIASDASEMANA = { "Sábado", "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira" }; //2022 começa em um sábado
-
-    // atributos 
+    //atributos 
     private int dia;
     private int mes;
-    private int ano;
+    private int ano; 
     //#endregion
     
     //#region Construtores   
@@ -22,17 +21,17 @@ public class Data {
      * @param mes Mês para a data
      * @param ano Ano para a data
      */
-    private void init(int dia, int mes, int ano) {
+    private void init(int dia, int mes, int ano){
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
 
-        if (!this.dataValida()) {
+        if (!this.dataValida()){
             this.dia = this.mes = 1;
             this.ano = 1900;
         }
 
-        if (eAnoBissexto())
+        if(eAnoBissexto())
             DIASDOMES[1] = 29;
     }
     
@@ -42,14 +41,14 @@ public class Data {
      * @param mes Mês para a data
      * @param ano Ano para a data
      */
-    public Data(int dia, int mes, int ano) {
+    public Data(int dia, int mes, int ano){
         this.init(dia, mes, ano);
     }
 
     /**
      * Construtor para data padrão: data atual
      */
-    public Data() {
+    public Data(){
         LocalDateTime dataAtual = LocalDateTime.now();
 
         this.init(dataAtual.getDayOfMonth(), dataAtual.getMonthValue(), dataAtual.getYear());
@@ -58,15 +57,27 @@ public class Data {
 
     //#region Métodos GET
 
-    public int getDia() {
+    /**
+     * Retorna o dia estipulado
+     * @return int com o dia
+     */
+    public int getDia(){
         return this.dia;
     }
 
-    public int getMes() {
+    /**
+     * Retorna o mes estipulado
+     * @return int com o mes
+     */
+    public int getMes(){
         return this.mes;
     }
 
-    public int getAno() {
+    /**
+     * Retorna o ano estipulado
+     * @return int com o ano
+     */
+    public int getAno(){
         return this.ano;
     }
     //#endregion
@@ -77,10 +88,11 @@ public class Data {
     * Verifica se o ano da data armazenada é bissexto.
     * @return Se o ano é bissexto (true) ou não (false)
     */
-    public boolean eAnoBissexto() {
-        if (this.ano % 400 == 0) 
+    public boolean eAnoBissexto(){
+
+        if(this.ano % 400 == 0) 
               return true;
-        else if (this.ano % 4 == 0 && this.ano % 100 != 0)
+        else if(this.ano % 4 == 0 && this.ano % 100 != 0)
               return true;
 
         return false;
@@ -90,12 +102,12 @@ public class Data {
     * Verifica se a data armazenada é válida (método privado). Só valem datas de 1900 em diante.
     * @return Se a data é válida (true) e se não é válida (false)
     */
-    private boolean dataValida() {
+    private boolean dataValida(){
         int maximoDeDias = 0;
 
-        if (this.ano < 1900)
+        if(this.ano < 1900)
             return false;
-        else {
+        else{
            if (this.mes < 1 || this.mes > 12)
                return false;
            else { 
@@ -109,29 +121,32 @@ public class Data {
 
         return true;               
     }
-
+    
     /**
      * Método booleano que compara o parâmetro do tipo Data com o atributo data do próprio objeto para verificar se é menor
      * @param data param do tipo Data
      * @return Se data parametrizada é menor (true) e se é igual ou maior (false)
      */
     public boolean eMenor(Data data){
-        if(this.getAno() == data.getAno() && this.getMes() == data.getMes() && this.getDia() == data.getDia())
+        if(this.getAno() == data.getAno() && this.getMes() == data.getMes() && this.getDia() == data.getDia()){
             return false;
-        else {
-            if (this.getAno() < data.getAno())
+        }else{
+            if (this.getAno() < data.getAno()){
                 return true;
-            else {
-                if (this.getMes() < data.getMes())
+            }else{
+                if (this.getMes() < data.getMes()){
                     return true;
-                else {
-                    if (this.getDia() < data.getDia())
+                }else{
+                    if(this.getDia() <data.getDia()){
                         return true;
-                    else
+                    }else{
                         return false;
+                    }
                 }
             }
         }
+        
+
     }
     //#endregion
 
@@ -141,16 +156,16 @@ public class Data {
      * Método que adiciona a quantidade de dias informada a partir da data estipulada
      * @param dias param do tipo inteiro
      */
-    public void adicionaDias(int dias) {
+    public void adicionaDias(int dias){
         int mes = DIASDOMES[(this.mes - 1) % 12];
 
-        if (dias > 0)
+        if(dias > 0)
             this.dia += dias;
 
-        if (this.dia > mes) {
-            while (this.dia > mes) {
+        if(this.dia > mes){
+            while (this.dia > mes){
 
-                if (eAnoBissexto()) { 
+                if(eAnoBissexto()){ 
                     DIASDOMES[1] = 29; 
                 } else {
                     DIASDOMES[1] = 28; 
@@ -159,7 +174,7 @@ public class Data {
                 this.dia -= mes;
                 this.mes++;
 
-                if (this.mes > 12) {
+                if(this.mes > 12){
                     this.ano++;
                     this.mes -= 12; 
                 }
@@ -173,7 +188,7 @@ public class Data {
      * Método que retorna a quantidade de dias no mês a partir da data informada
      * @return quantidade de dias no mês
      */
-    public int diasNoMes() {
+    public int diasNoMes(){
         return DIASDOMES[this.mes];
     }
 
@@ -182,12 +197,13 @@ public class Data {
      * Datas inválidas retornam e são calculadas como 01/01/2022
      * @return O dia da semana da data parametrizada.
      */
-    public String diaDaSemana() {
+    public String diaDaSemana(){
+
         int deslocamento = 0;
         int totalDias = 0;
         int mesParaSomar = 1;
         
-        while (mesParaSomar < this.mes) {
+        while(mesParaSomar < this.mes){
             totalDias += DIASDOMES[mesParaSomar - 1];
             mesParaSomar++;
         }
@@ -201,12 +217,13 @@ public class Data {
     /**
      * Método que adiciona um dia à data correspondente
      */
-    public void proximoDia() {
-        if (this.dia == DIASDOMES[this.mes - 1]) {
-            if (this.mes == 12) {
+    public void proximoDia(){
+        if(this.dia == DIASDOMES[this.mes - 1]){
+            if(this.mes == 12){
 
                 this.ano++;
                 this.mes = 1;
+    
             } else 
                 this.mes++;
             
@@ -219,7 +236,8 @@ public class Data {
     * Retorna a data formatada
     * @return String com a data no formato dd/mm/aaaa
     */
-    public String dataFormatada() {
+    public String dataFormatada(){
+        
         return (String.format("%02d",this.dia)+ 
           "/" + String.format("%02d",this.mes)+ 
           "/" + String.format("%4d",this.ano));
